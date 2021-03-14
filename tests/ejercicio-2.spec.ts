fbf102eeb7626e1a4cf40eddd18e996d43f123ac
XueMei-L <72436762+XueMei-L@users.noreply.github.com>
2021-03-14 22:03:41 +0100

    Delete tests directory

--- tests/ejercicio-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import "mocha";
-import {expect} from "chai";
-import {meshArray} from "../src/ejercicio-2";
-
-describe("Ejercicio 02: prueba", () => {
-  it("meshArray([´allow”, ´lowering”, ´ringmaster”, ´terror”]) returns value lowringter", () => {
-    expect(meshArray(["allow", "lowering", "ringmaster", "terror"])).to.be.equal("lowringter");
-  });
-});
